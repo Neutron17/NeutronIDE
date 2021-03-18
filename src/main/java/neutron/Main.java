ab88527b4e65b7c28eccec49b6dca6422f812c8f
Neutron17 <sandormatyas17@gmail.com>
2021-03-18 23:45:12 +0100

    ""

--- src/main/java/neutron/Main.java
+++ src/main/java/neutron/Main.java
@@ -6,23 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application { // --module-path C:\Users\sando\Downloads\javafx-sdk-15.0.1\lib --add-modules=javafx.controls,javafx.fxml
     public static Stage stage;
     public static Scene scene;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        System.out.println("Henlo?");
         stage = primaryStage;
         stage.setTitle("Neutron IDE");
         stage.setMaximized(true);
-        System.out.println("parent");
         Parent root = FXMLLoader.load(getClass().getResource("/editor.fxml"));
-        System.out.println("parent");
         scene = new Scene(root);
         stage.setScene(scene);
-        System.out.println("Show?");
         stage.show();
-        System.out.println("End?");
     }
 // --module-path C:\Users\sando\Downloads\javafx-sdk-15.0.1\lib --add-modules=javafx.controls,javafx.fxml
 
