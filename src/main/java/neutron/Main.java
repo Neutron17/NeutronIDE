d1da9deeb7d07c9b788e464dd35b894e707caffe
Neutron17 <sandormatyas17@gmail.com>
2021-03-18 23:36:48 +0100

    ""

--- src/main/java/neutron/Main.java
+++ src/main/java/neutron/Main.java
@@ -5,21 +5,24 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import neutron.controller.SettingsController;
-import neutron.pass.WhiteSpace;
 
 public class Main extends Application {
     public static Stage stage;
     public static Scene scene;
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Henlo?");
         stage = primaryStage;
         stage.setTitle("Neutron IDE");
         stage.setMaximized(true);
-        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        System.out.println("parent");
+        Parent root = FXMLLoader.load(getClass().getResource("/editor.fxml"));
+        System.out.println("parent");
         scene = new Scene(root);
         stage.setScene(scene);
+        System.out.println("Show?");
         stage.show();
+        System.out.println("End?");
     }
 // --module-path C:\Users\sando\Downloads\javafx-sdk-15.0.1\lib --add-modules=javafx.controls,javafx.fxml
 
--- src/main/java/neutron/controller/MainController.java
+++ src/main/java/neutron/controller/MainController.java
@@ -7,18 +7,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import neutron.io.Reader;
 import neutron.io.Writer;
+import org.jetbrains.annotations.NotNull;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
@@ -37,7 +40,8 @@ public class MainController implements Initializable {
     Label outLabel = new Label();
     @FXML
     TextArea outField = new TextArea();
-
+    @FXML
+    TreeView<String> filesTreeView = new TreeView<String>();
 
     @FXML
     public void handleClose() {
@@ -63,7 +67,7 @@ public class MainController implements Initializable {
     @FXML
     public void handleClear() { outField.clear(); }
 
-    public void cmd(String command) {
+    public void cmd(@NotNull String command) {
         Writer.bufferedWriter("",System.getProperty("user.dir")+"\\test.c");
         try {
 //            Pattern pattern = Pattern.compile(".[\"]");
@@ -78,7 +82,7 @@ public class MainController implements Initializable {
 //                for (int i = 0;i<list.size();i++) {
 //                    System.out.println(list.get(i));
 //                }
-                run("cmd.exe","/c","gcc text.c");
+                run("cmd.exe","/c","gcc test.c");
                 run("cmd.exe","/c",".\\a.exe");
             }else if(IS_MAC) {
               //  builder.command("/bin/bash","-c","ls");
@@ -112,7 +116,7 @@ public class MainController implements Initializable {
             e.printStackTrace();
         }
     }
-    public void run(String... arg) {
+    public void run(@NotNull String... arg) {
         try {
             ProcessBuilder builder = new ProcessBuilder(arg);
             Process p = builder.start();
@@ -124,16 +128,40 @@ public class MainController implements Initializable {
                 if (line == null) {
                     break;
                 }
-                outField.setText(outField.getText() + line + "\n");
+                //outField.setText(outField.getText() + line + "\n");
                 System.out.println(line);
             }
             p.waitFor();
             buff.close();
-            p.destroy();
+            p.destroyForcibly();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
     @Override
-    public void initialize(URL location, ResourceBundle resources) { }
+    public void initialize(URL location, ResourceBundle resources) {
+        filesTreeView.setEditable(false);
+        TreeItem<String> root = new TreeItem<>(new File(".").getName());
+        root.setExpanded(true);
+        direct(root);
+        filesTreeView.setRoot(root);
+    }
+    void direct(TreeItem<String> root) {
+        for(File i : Objects.requireNonNull(new File(".").listFiles())) {
+            TreeItem<String> current = new TreeItem<String>(i.getName());
+            root.getChildren().add(current);
+            if(i.isDirectory()) {
+                current.setGraphic(new ImageView(new Image("/images/empty_directory.png"))); // TODO Icon made by https://www.flaticon.com/authors/dinosoftlabs from @flaticon
+                while(i.listFiles() != null) {
+
+                }
+            }else {
+                // TODO Different images for file extensions
+                ImageView fileIcon = new ImageView(new Image("/images/file.png"));
+                //fileIcon.setFitHeight(20);
+                //fileIcon.setFitWidth(20);
+                current.setGraphic(fileIcon);
+            }
+        }
+    }
 }
